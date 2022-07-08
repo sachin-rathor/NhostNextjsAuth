b4307a666d5e55a71af062cbbb6fd1dffa119c1c
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2022-07-08 17:53:09 +0530

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -6,6 +6,8 @@ import Head from 'next/head';
 import Layout from '../components/Layout';
 import Input from '../components/Input';
 
+import withAuth from '../withAuth';
+
 const Profile = () => {
   const { user } = useUserContext();
 
@@ -77,4 +79,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default withAuth(Profile);
